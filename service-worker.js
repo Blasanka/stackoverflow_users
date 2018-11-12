98543039250b2eabdae6c855acc695e392b87cb4
blasanka <blasanka95@gmail.com>
2018-11-12 19:08:07 +0530

    heroku hompepage set with static.json

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "blasanka.github.io/stackoverflow_users/precache-manifest.6802e2fb2a7880794294f2d8c1d9fb66.js"
+  "/precache-manifest.65a573576ab944f38201d889050cf6b7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("blasanka.github.io/stackoverflow_users/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

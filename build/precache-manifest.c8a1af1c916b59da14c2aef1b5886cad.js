a161611243f97aae794e3c85948ca130b78d5c9c
blasanka <blasanka95@gmail.com>
2018-11-12 14:44:19 +0530

    rebuilt website

---
+++ build/precache-manifest.c8a1af1c916b59da14c2aef1b5886cad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "84f308e8703d5f8c96d959d98d278ae2",
+    "url": "/static/media/silver.84f308e8.png"
+  },
+  {
+    "revision": "2304987e4f04beb2aff3ba9f83a13718",
+    "url": "/static/media/gold.2304987e.png"
+  },
+  {
+    "revision": "01ac67fc06a0ac2872416d0e6954df36",
+    "url": "/static/media/expressway rg.01ac67fc.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1356f9f29d506a7ca97",
+    "url": "/static/js/main.e1356f9f.chunk.js"
+  },
+  {
+    "revision": "1375650ceec063e7dc85",
+    "url": "/static/js/1.1375650c.chunk.js"
+  },
+  {
+    "revision": "e1356f9f29d506a7ca97",
+    "url": "/static/css/main.8d41a8ed.chunk.css"
+  },
+  {
+    "revision": "1375650ceec063e7dc85",
+    "url": "/static/css/1.cbcdab1c.chunk.css"
+  },
+  {
+    "revision": "1392ee0d3a0ca516471d6b434f28f57d",
+    "url": "/index.html"
+  }
+];

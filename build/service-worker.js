a626b6c53919b98444542e20571be4ffcc3332c5
blasanka <blasanka95@gmail.com>
2018-11-24 22:57:20 +0530

    rebuild website

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e7d12613aca6f0853237becedaf2f72.js"
+  "blasanka.github.io/stackoverflow_users/precache-manifest.5aff67d9f59bb284f529d86507a9d13d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("blasanka.github.io/stackoverflow_users/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
